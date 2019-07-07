17f7e446727072defc4aaa885c6181f1e8907666
Lloyd <lloyd.mckie@outlook.com>
2019-07-06 19:01:11 -0700

    Handle disposing of printer list

--- Public/Update-Printer.ps1
+++ Public/Update-Printer.ps1
@@ -74,7 +74,7 @@ function Update-Printer {
         $NewPrinterName = '{0} {1}' -f $Name, $i
 
         $maxPrinterIterations = 100
-        while(get-printer -name $NewPrinterName -ErrorAction SilentlyContinue) { 
+        if(get-printer -name $NewPrinterName -ErrorAction SilentlyContinue) { 
             $NewPrinterName = '{0} {1}' -f $Name, $i
             $i++ 
             if($i -ge $maxPrinterIterations ) {
@@ -84,57 +84,63 @@ function Update-Printer {
         }
     }
     # $AsJob = $AsJob
+    try {
+        # TODO Use Get-PrinterHelper
+        $printer = Get-Printer @printerArgs
 
-    # TODO Use Get-PrinterHelper
-    $printer = Get-Printer @printerArgs
+        if($AsNewPrinter) {
+            if($printer -and [string]::IsNullOrEmpty($printer.Name)) {
+                Write-Error "To many printers returned"
+                Write-Host $printer
+            } else {
 
-    if($AsNewPrinter) {
-        if($printer -and [string]::IsNullOrEmpty($printer.Name)) {
-            Write-Error "To many printers returned"
-            Write-Host $printer
-        } else {
+                # Treat it as multiple for now
+                $printer | % { 
+                    $p = $_
+                    # Name, ComputerName, Type, DriverName, PortName, Shared, Published
 
-            # Treat it as multiple for now
-            $printer | % { 
-                $p = $_
-                # Name, ComputerName, Type, DriverName, PortName, Shared, Published
+                    $newPrinterArgs = @{}
+                    
+                    # Basic example
+                    ## source https://docs.microsoft.com/en-us/powershell/module/printmanagement/add-printer?view=win10-ps
+                    # Add-Printer -Name "mxdw 2" -DriverName "Microsoft XPS Document Writer v4" -PortName "portprompt:"
 
-                $newPrinterArgs = @{}
+                    $newPrinterArgs.Add("Name", $NewPrinterName)
+                    $newPrinterArgs.Add("DriverName", $p.DriverName)
+                    $newPrinterArgs.Add("PortName", $p.PortName)
+
+                    if($Share) {
+                        Write-Host "Sharing printer $ShareName"
+                        $newPrinterArgs.Add("Shared", $true)
+                        $newPrinterArgs.Add("ShareName", $ShareName)
+                    }
+                    Add-Printer @newPrinterArgs
+                    # Note: Three valid sets of implementations that are distict
+
+                    # Add-Printer [-ConnectionName] <string> [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf]
+                    # [-Confirm]  [<CommonParameters>]
                 
-                # Basic example
-                ## source https://docs.microsoft.com/en-us/powershell/module/printmanagement/add-printer?view=win10-ps
-                # Add-Printer -Name "mxdw 2" -DriverName "Microsoft XPS Document Writer v4" -PortName "portprompt:"
-
-                $newPrinterArgs.Add("Name", $NewPrinterName)
-                $newPrinterArgs.Add("DriverName", $p.DriverName)
-                $newPrinterArgs.Add("PortName", $p.PortName)
-
-                if($Share) {
-                    Write-Host "Sharing printer $ShareName"
-                    $newPrinterArgs.Add("Shared", $true)
-                    $newPrinterArgs.Add("ShareName", $ShareName)
+                    # Add-Printer [-Name] <string> [-Comment <string>] [-Datatype <string>] [-UntilTime <uint32>] [-KeepPrintedJobs]
+                    # [-Location <string>] [-SeparatorPageFile <string>] [-ComputerName <string>] [-Shared] [-ShareName <string>]
+                    # [-StartTime <uint32>] [-PermissionSDDL <string>] [-PrintProcessor <string>] [-Priority <uint32>] [-Published]
+                    # [-RenderingMode {SSR | CSR | BranchOffice}] [-DisableBranchOfficeLogging] [-BranchOfficeOfflineLogSizeMB
+                    # <uint32>] [-WorkflowPolicy {Uninitialized | Disabled | Enabled}] [-DeviceURL <string>] [-DeviceUUID <string>]
+                    # [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf] [-Confirm]  [<CommonParameters>]
+                
+                    # Add-Printer [-Name] <string> [-DriverName] <string> -PortName <string> [-Comment <string>] [-Datatype <string>]
+                    # [-UntilTime <uint32>] [-KeepPrintedJobs] [-Location <string>] [-SeparatorPageFile <string>] [-ComputerName
+                    # <string>] [-Shared] [-ShareName <string>] [-StartTime <uint32>] [-PermissionSDDL <string>] [-PrintProcessor
+                    # <string>] [-Priority <uint32>] [-Published] [-RenderingMode {SSR | CSR | BranchOffice}]
+                    # [-DisableBranchOfficeLogging] [-BranchOfficeOfflineLogSizeMB <uint32>] [-WorkflowPolicy {Uninitialized |
+                    # Disabled | Enabled}] [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf] [-Confirm]
+                    # [<CommonParameters>]
                 }
-                Add-Printer @newPrinterArgs
-                # Note: Three valid sets of implementations that are distict
-
-                # Add-Printer [-ConnectionName] <string> [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf]
-                # [-Confirm]  [<CommonParameters>]
-            
-                # Add-Printer [-Name] <string> [-Comment <string>] [-Datatype <string>] [-UntilTime <uint32>] [-KeepPrintedJobs]
-                # [-Location <string>] [-SeparatorPageFile <string>] [-ComputerName <string>] [-Shared] [-ShareName <string>]
-                # [-StartTime <uint32>] [-PermissionSDDL <string>] [-PrintProcessor <string>] [-Priority <uint32>] [-Published]
-                # [-RenderingMode {SSR | CSR | BranchOffice}] [-DisableBranchOfficeLogging] [-BranchOfficeOfflineLogSizeMB
-                # <uint32>] [-WorkflowPolicy {Uninitialized | Disabled | Enabled}] [-DeviceURL <string>] [-DeviceUUID <string>]
-                # [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf] [-Confirm]  [<CommonParameters>]
-            
-                # Add-Printer [-Name] <string> [-DriverName] <string> -PortName <string> [-Comment <string>] [-Datatype <string>]
-                # [-UntilTime <uint32>] [-KeepPrintedJobs] [-Location <string>] [-SeparatorPageFile <string>] [-ComputerName
-                # <string>] [-Shared] [-ShareName <string>] [-StartTime <uint32>] [-PermissionSDDL <string>] [-PrintProcessor
-                # <string>] [-Priority <uint32>] [-Published] [-RenderingMode {SSR | CSR | BranchOffice}]
-                # [-DisableBranchOfficeLogging] [-BranchOfficeOfflineLogSizeMB <uint32>] [-WorkflowPolicy {Uninitialized |
-                # Disabled | Enabled}] [-CimSession <CimSession[]>] [-ThrottleLimit <int>] [-AsJob] [-WhatIf] [-Confirm]
-                # [<CommonParameters>]
             }
+        }
+    } finally {
+        if($printer) { # TODO Handle the other get-printer calls
+            Write-Host "Disposing of list of printers"
+            $printer.Dispose()
         }
     }
 }
